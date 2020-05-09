dab3c701ee93849060c8bb0cc8fc7ea90f615c6e
s19171 <61733699+s19171@users.noreply.github.com>
2020-05-09 14:22:46 +0200

    Initial commit

---
+++ LinqConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            d.Przyklad1();
+            Console.WriteLine("");
+            d.Przyklad2();
+            Console.WriteLine("");
+            d.Przyklad3();
+            Console.WriteLine("");
+            d.Przyklad4();
+            Console.WriteLine("");
+            d.Przyklad5();
+            Console.WriteLine("");
+            d.Przyklad6();
+            Console.WriteLine("");
+            d.Przyklad7();
+            Console.WriteLine("");
+            d.Przyklad8();
+            Console.WriteLine("");
+            d.Przyklad9();
+            Console.WriteLine("");
+            d.Przyklad10Button_Click();
+            Console.WriteLine("");
+            d.Przyklad11();
+            Console.WriteLine("");
+            d.Przyklad12();
+        }
+    }
+}
